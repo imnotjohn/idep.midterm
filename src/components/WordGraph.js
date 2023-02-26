64b980ed1ed854d8a0185ba00c020c6c339c0234
Jon Chinen <j.chinen@berkeley.edu>
2023-02-26 15:20:09 -0500

    Implemented Edges + Revert to CSS2DObjects.

--- src/components/WordGraph.js
+++ src/components/WordGraph.js
@@ -4,8 +4,10 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GUI} from 'three/addons/libs/lil-gui.module.min.js';
 import './css/Graph.css';
 
+import SIMSDATA from '../lib/SimMatData';
 import WORDS from '../lib/SimWords';
 import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+// import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 
 
 import {WG, WN, WE} from '../lib/WordGraphHelper';
@@ -21,18 +23,22 @@ const WordGraph = () => {
 
         // instancing test
         let instance;
+        let lineMaterial;
         const _dummy = new THREE.Object3D();
         const _matrix = new THREE.Matrix4();
         const _quaternion = new THREE.Quaternion();
         const _scale = new THREE.Vector3(1, 1, 1);
         const _axis = new THREE.Vector3();
         const _position = new THREE.Vector3();
+        const _points = [];
+        const nScale = 60;
 
         let labelRenderer;
-        let testMesh;
+        // let testMesh;
 
         const params = {
-            nodeCount: 300,
+            nodeCount: 10,
+            threshold: 0.55,
         }
 
         const init = () => {
@@ -51,15 +57,19 @@ const WordGraph = () => {
             renderer.setSize( window.innerWidth, window.innerHeight );
             document.body.appendChild( renderer.domElement );
 
-            // test
-            labelRenderer = new CSS2DRenderer();
+            // 2D test
+            labelRenderer = new CSS2DRenderer(); // 2D
             labelRenderer.setSize(window.innerWidth, window.innerHeight);
             labelRenderer.domElement.style.position = "absolute";
             labelRenderer.domElement.style.top = "0px";
             document.body.appendChild(labelRenderer.domElement);
+            // 3D test
+            // labelRenderer = new CSS3DRenderer({alpha: true});
+            // labelRenderer.setSize(window.innerWidth, window.innerHeight);
+            // document.querySelector("#App").appendChild(labelRenderer.domElement);
 
             // controls = new OrbitControls(camera, renderer.domElement);
-            controls = new OrbitControls(camera, labelRenderer.domElement);
+            controls = new OrbitControls(camera, labelRenderer.domElement); // 2D
             controls.minDistance = 50;
             controls.maxDistance = 500;
             controls.target.set(0, 0, 0);
@@ -74,11 +84,14 @@ const WordGraph = () => {
             instance.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
             // instance.count = 0;
 
-            const testGeom = new THREE.SphereGeometry(3, 32, 16);
-            const testMaterial = new THREE.MeshPhongMaterial({color: 0xFF0000});
-            testMesh = new THREE.Mesh(testGeom, testMaterial);
+            // Line Object
+            lineMaterial = new THREE.LineBasicMaterial({color: 0xFF0000, transparent: true, opacity: 0.45});
+
+            // const testGeom = new THREE.SphereGeometry(3, 32, 16);
+            // const testMaterial = new THREE.MeshPhongMaterial({color: 0xFF0000});
+            // testMesh = new THREE.Mesh(testGeom, testMaterial);
             
-            scene.add(instance, testMesh);
+            scene.add(instance);
 
             // set up GUI
             const gui = new GUI();
@@ -104,6 +117,7 @@ const WordGraph = () => {
         }
 
         const initLabel = (node) => {
+            // 2D
             const nodeDiv = document.createElement("div");
             nodeDiv.className = "label";
             nodeDiv.textContent = node.w;
@@ -111,14 +125,20 @@ const WordGraph = () => {
             const nodeLabel = new CSS2DObject(nodeDiv);
             nodeLabel.position.set(node.p.x, node.p.y, node.p.z);
             instance.add(nodeLabel);
+
+            // 3D
+            // const nodeDiv = document.createElement("div");
+            // nodeDiv.className = "label";
+            // nodeDiv.textContent = node.w;
+            // nodeDiv.style.marginTop = "-1em";
+            // const nodeCSSObj = new CSS3DObject(nodeDiv);
+            // nodeCSSObj.position.set(node.p.x-80, node.p.y+80, node.p.z-600);
+            // instance.add(nodeCSSObj);
         }
 
-        // const nNodes = 20;
-        const rnd = Math.random();
-        const nScale = 60;
         const initNodes = () => {
             for (let i = 0; i < params.nodeCount; i++) {
-                g.nodes.push(new WN(new THREE.Vector3(Math.random() * (Math.random() * 1.75) * nScale, Math.random() * (Math.random() * 1.75) *nScale, Math.random() * (Math.random() * 1.75) * nScale), WORDS[i]));
+                g.nodes.push(new WN(new THREE.Vector3(Math.random() * (Math.random() * 1.75) * nScale, Math.random() * (Math.random() * 1.75) * nScale, Math.random() * (Math.random() * 1.75) * nScale), WORDS[i]));
             }
 
             for (let i = 0; i < params.nodeCount; i++) {
@@ -137,22 +157,44 @@ const WordGraph = () => {
             setTargetAverage();
         }
 
-/*
         const initEdges = () => {
-            // implement edges
-            // const lmat = new THREE.LineBasicMaterial({color: 0xFF0000, transparent: true, opacity: 0.45});
+            for (let j = 0; j < params.nodeCount; j++) {
+                const row = SIMSDATA[j];
+                for (let i = j + 1; i < params.nodeCount; i++) {
+                    const e = new WE(g.nodes[j], g.nodes[i])
+                    g.edges.push(e);
+                    const sim = row[i];
+                    if (sim < params.threshold) {
+                        e.k = 0.05;
+                        e.targetLength = (1.0 - sim) * nScale * 2.0;
+                        e.show = false;
+                    } else {
+                        _points.push(g.nodes[j].p)
+                        e.k = 0.5;
+                        e.targetLength = (1.0 - sim) * nScale;
+                        e.show = true;
+                    }
+                }
+            }
 
-            // for (let i = 0; i < g.edges.length; i++) {
-            //     if (g.edges[i].show) {
-            //         const pts = [g.edges[i].n0.p, g.edges[i].n1.p];
-            //         const geo = new THREE.BufferGeometry().setFromPoints(pts);
-            //         const line = new THREE.Line(geo, lmat);
-                    
-            //         scene.add(line);
-            //     }
-            // }
+            // scene.add(new THREE.Line(lineGeom, lineMaterial));
+            updateEdges();
         }
-*/
+
+        const updateEdges = () => {
+            // update edges connections
+            const lineGeom = new THREE.BufferGeometry().setFromPoints(_points);
+
+            for (let i = 0; i < g.edges.length; i++) {
+                if (g.edges[i].show) {
+                    const pts = [g.edges[i].n0.p, g.edges[i].n1.p];
+                    const geo = new THREE.BufferGeometry().setFromPoints(pts);
+                    const line = new THREE.Line(geo, lineMaterial);
+                    
+                    scene.add(line);
+                }
+            }
+        }
 
         const onWindowResize = () => {
             camera.aspect = window.innerWidth / window.innerHeight;
@@ -171,7 +213,7 @@ const WordGraph = () => {
             }
 
             renderer.render(scene, camera);
-            labelRenderer.render(scene, camera);
+            labelRenderer.render(scene, camera); // 2D
         }
 /*
         const move = () => {
@@ -195,6 +237,7 @@ const WordGraph = () => {
 
         init();
         initNodes();
+        initEdges();
         // move();
         animate();
 
